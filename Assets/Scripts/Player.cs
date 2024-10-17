8098e378f7cdb1245c12a247142962a06de812c5
luiigi9 <luisdelgadoc.09@gmail.com>
2024-10-17 21:15:54 +0200

    Inicio

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speedMovement;
+    CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector2 movimento = new Vector2(h, v).normalized;
+        if (movimento.magnitude > 0)
+        {
+            float angleRotazzione = Mathf.Atan2(movimento.x, movimento.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+            transform.eulerAngles = new Vector3(0, angleRotazzione, 0);
+            Vector3 movement = Quaternion.Euler(0, angleRotazzione, 0) * Vector3.forward;
+            cc.Move(movement * speedMovement * Time.deltaTime);
+        }
+        
+        
+    }
+}
