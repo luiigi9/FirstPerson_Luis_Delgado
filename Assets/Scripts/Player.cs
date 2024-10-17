582893412df2325cfab52c6af17bf29985cdbfba
luiigi9 <luisdelgadoc.09@gmail.com>
2024-10-17 21:20:20 +0200

    InicioPlus

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         cc = GetComponent<CharacterController>();
     }
 
